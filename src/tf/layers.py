b9c54fc432798dc724e94070c43f25ff8d320ced
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-19 12:02:58 +0300

    add a full conv net with layers

--- src/tf/layers.py
+++ src/tf/layers.py
@@ -5,7 +5,12 @@ import tensorflow.keras.layers as tfl
 model = tf.keras.Sequential([
     tfl.ZeroPadding2D(padding=3, input_shape=(64, 64, 3)),
     tfl.Conv2D(filters=32, kernel_size=(7, 7), strides=1, padding="valid"),
-
+    tfl.BatchNormalization(axis=-1),  # use batch norm along the channel axis
+    tfl.ReLU(),
+    # keep moving in 2x2 size within the max pool layer
+    tfl.MaxPool2D(pool_size=(2, 2), strides=None, padding='valid'),
+    tfl.Flatten(),
+    tfl.Dense(units=1, activation="sigmoid")
 ])
 
 # Display the model's architecture
