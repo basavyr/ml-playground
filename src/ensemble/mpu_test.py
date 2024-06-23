3d5b8eb588a09e8f30e4e14c332ef9dc1814d514
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-23 13:19:33 +0300

    improve the mpu test

--- src/ensemble/mpu_test.py
+++ src/ensemble/mpu_test.py
@@ -1,10 +1,18 @@
 import torch
-if torch.backends.mps.is_available():
-    mps_device = torch.device("mps")
-    x = torch.ones(1, device=mps_device)
-    print(torch.cuda.is_available())
-    print(dir(torch.device))
-    print('MPS device found')
-    print (x)
+
+
+# Check that MPS is available
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
 else:
-    print ("MPS device not found.")
+    mps_device = torch.device("mps")
+
+    # Create a Tensor directly on the mps device
+    x = torch.ones(5, device=mps_device)
+    print('Apple NPU is ready')
+    print(x)
