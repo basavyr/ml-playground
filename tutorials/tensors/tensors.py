e28756ca81b8c5c88d7e5d5b7f65f7bc3367c1ac
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-24 16:28:41 +0300

    generate an image with rgb channels

--- tutorials/tensors/tensors.py
+++ tutorials/tensors/tensors.py
@@ -1,1 +1,27 @@
 # source: https://www.youtube.com/watch?v=IC0_FRiX-sw
+
+import torch
+
+
+def generate_grid(n_samples, image_size):
+    """
+    - generate an image of size `height x width` which has a fixed number of channels (provided by local `n_channels` variable)
+    """
+    n_channels = 3
+    height, width = image_size
+    return torch.rand(n_samples, n_channels, height, width)
+
+
+def generate_data(n_samples: int, image_size: tuple[int, int]):
+    return generate_grid(n_samples, image_size)
+
+
+if __name__ == "__main__":
+
+    m = 5
+    image_size = (2, 2)
+
+    images = generate_grid(m, image_size)
+    for idx, grid in enumerate(images):
+        print(f'Image {idx+1}')
+        print(grid)
