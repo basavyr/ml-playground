cfe36935125c8671d35e295e29c76f2d4dcd0405
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-24 15:04:04 +0300

    sketch the training function

--- tutorials/build_nn/build_nn.py
+++ tutorials/build_nn/build_nn.py
@@ -8,6 +8,10 @@ from torchvision import datasets, transforms
 import torch.functional
 
 
+import model as m
+import data_loader as dl
+
+
 def get_device():
 
     device = (
@@ -21,11 +25,53 @@ def get_device():
     return device
 
 
-def main():
-    # in order to have a model we need to import nn from pytorch
-    model = nn.Sequential(nn.Linear(10, 10))
+def get_model(input_size, output_size, params, device):
+    model = m.Model(input_size, output_size, params).to(device)
     return model
 
 
-if __name__ == "__main__":
+def init_model_parameters():
+    n_samples = 1000
+    input_size = 250
+    output_size = 3
+    params = [1, 1]
+    return n_samples, input_size, output_size, params
+
+
+def test_tensor(model):
     device = get_device()
+    n_samples, input_size, output_size, params = init_model_parameters()
+    X = torch.rand(n_samples, input_size, input_size, device=device)
+
+    # evaluate the model
+    Y_pred = model(X)
+
+    print(f"Predicted class: {Y_pred}")
+
+    print(f"Model structure: {model}\n\n")
+
+
+def train(train_dataloader, model, loss_function, optimizer):
+    print(model)
+
+    exit(1)
+
+
+def main():
+    # get device
+    device = get_device()
+    n_samples, input_size, output_size, params = init_model_parameters()
+
+    # automatically download the data sets and create the loaders for both training and test data
+    train_dataloader, test_dataloader = dl.get_loaders(batch_size=128)
+
+    model = get_model(input_size, output_size, params, device)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters())
+
+    train(train_dataloader, model, loss_fn, optimizer)
+
+
+if __name__ == "__main__":
+    main()
