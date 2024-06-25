17866f3a344059b10dce713e15a2b5734f7968ee
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-06-25 10:06:02 +0300

    add a tensor generator
    also supports tensor generator from a python struct

--- tutorials/tensors/t_ops.py
+++ tutorials/tensors/t_ops.py
@@ -7,33 +7,39 @@ import math
 import numpy as np
 
 
+class TensorEnvironments:
+    def __init__(self, param):
+        self.param = param
+
+    def generate_numpy_array(self, M, N, H, W) -> np.ndarray:
+        return np.random.rand(M, N, H, W)
+
+    def test_tensors(self):
+        t0 = torch.tensor([1, 1])
+        t = torch.rand([2, 2])
+        t_np = torch.from_numpy(self.generate_numpy_array(3, 3, 3, 3))
+        # create a diagonal tensor (here is just a 2D matrix) that will have on the diagonal the initial fixed-length tensor (i.e., here it is denoted by `t0`)
+        t2 = torch.diag(t0, diagonal=0)
+        print(f't -> {t}')
+        print(f't_np -> {t_np}')
+        print(f't2 -> {t2}')
+        print(f't + t2 -> {t+t2}')
+        print(f't + t2 -> {t*t2}')
+        # print(f't / t2 -> {t.mm(inv(t2))}')
+
+
 def init_params():
     M, N, H, W = (100, 3, 28, 28)
     return M, N, H, W
 
 
-def generate_tensor(M, N, H, W):
-    t0 = torch.tensor([1, 1])
-    t = torch.rand([2, 2])
-    t_np = torch.from_numpy(generate_numpy_array(3))
-    # create a diagonal tensor (here is just a 2D matrix) that will have on the diagonal the initial fixed-length tensor (i.e., here it is denoted by `t0`)
-    t2 = torch.diag(t0, diagonal=0)
-    print(f't -> {t}')
-    print(f't_np -> {t_np}')
-    print(f't2 -> {t2}')
-    print(f't + t2 -> {t+t2}')
-    print(f't + t2 -> {t*t2}')
-    # print(f't / t2 -> {t.mm(inv(t2))}')
+def generate_tensor(device, M, N, H, W):
+    """
+    - generate a tensor that will be also allocated on a specific device
+    """
+    return torch.rand([M, N, H, W]).to(device)
 
 
-def generate_numpy_array(size: int) -> np.ndarray:
-    return np.random.rand(size, size)
-
-
-def main():
-    M, N, H, W = init_params()
-    generate_tensor(M, N, H, W)
-
-
-if __name__ == "__main__":
-    main()
+def generate_tensor_np(device, M, N, H, W):
+    t_np = np.random.rand(M, N, H, W)
+    return torch.from_numpy(t_np).to(device)
