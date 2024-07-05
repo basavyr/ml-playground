30c77cb5b0e9b56e70b91c816f64aeaf10bf9d0d
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-07-05 11:27:24 +0300

    add plot with acc and loss. support for device as arg

--- tutorials/bob_net/net.py
+++ tutorials/bob_net/net.py
@@ -17,16 +17,16 @@ import torch.optim as optim
 
 import matplotlib.pyplot as plt
 
-
-import tqdm
+import sys
+from tqdm import trange
 
 
 class Net(nn.Module):
     def __init__(self, input_size: int):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 10)
+        self.fc1 = nn.Linear(input_size, 512)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 10)
 
     def forward(self, x: torch.tensor):
         x = x.view(x.size(0), -1)  # flatten the input
@@ -46,7 +46,7 @@ def train_model(model: nn.Module, device: torch.device, data_loader: DataLoader,
     epoch_losses = []
     epoch_accs = []
     once = False
-    for epoch in range(num_epochs):
+    for epoch in (t := trange(num_epochs)):
         running_loss = 0
         correct_labels_per_epoch = 0
         total_labels = 0
@@ -78,13 +78,20 @@ def train_model(model: nn.Module, device: torch.device, data_loader: DataLoader,
             running_loss += loss.item()
             loss.backward()
             optimizer.step()
-        epoch_losses.append(running_loss / len(data_loader))
-        epoch_accs.append(float(correct_labels_per_epoch/total_labels))
-    for idx, data in enumerate(zip(epoch_losses, epoch_accs)):
-        loss, acc = data
-        print(f'Epoch: {idx + 1}')
-        print(f'Loss: {loss}')
-        print(f'Acc: {acc}\n')
+        epoch_loss = round(float(running_loss / len(data_loader)), 3)
+        epoch_acc = round(float(correct_labels_per_epoch/total_labels), 3)
+        epoch_losses.append(epoch_loss)
+        epoch_accs.append(epoch_acc)
+        t.set_description(f"loss {epoch_loss} acc {epoch_acc}")
+
+    plt.plot(epoch_losses)
+    plt.plot(epoch_accs)
+    plt.savefig("loss_acc.pdf", dpi=300, bbox_inches="tight")
+    # for idx, data in enumerate(zip(epoch_losses, epoch_accs)):
+    #     loss, acc = data
+    #     print(f'Epoch: {idx + 1}')
+    #     print(f'Loss: {loss}')
+    #     print(f'Acc: {acc}\n')
 
     torch.save(model, 'model.pth')
 
@@ -118,15 +125,16 @@ def test_model(model: nn.Module, device: torch.device, data_loader: DataLoader, 
                 break
 
 
-def main():
+def main(device_as_input_string: str):
     height = 28
     width = 28
     n_channels = 1
     input_size = height*width
     n_samples = 1
     batch_size = 128
+    num_epochs = 20
 
-    device = torch.device("mps")
+    device = torch.device(device_as_input_string)
 
     model = Net(input_size)
     model.to(device)
@@ -146,7 +154,7 @@ def main():
 
     if not os.path.exists("model.pth"):
         print('Training the model')
-        train_model(model, device, train_loader, 10)
+        train_model(model, device, train_loader, num_epochs)
     else:
         print('Model is already trained')
 
@@ -155,4 +163,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        device_as_input_string = sys.argv[1]
+    if len(sys.argv) == 3:
+        os.remove("model.pth")
+    main(device_as_input_string)
