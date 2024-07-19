3badb63d622f7064d984e2bfc4377e382ce295b0
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-07-19 17:51:02 +0300

    play with the weights of a pre trained model

---
+++ src/pre-trained/pre.py
@@ -0,0 +1,17 @@
+from torchvision.models import resnet50, ResNet50_Weights
+
+
+RN50_Weights = [ResNet50_Weights.IMAGENET1K_V1,
+                ResNet50_Weights.IMAGENET1K_V2, ResNet50_Weights.DEFAULT, None]
+
+
+def weights(model_weights: ResNet50_Weights):
+
+    model = resnet50(weights=model_weights)
+
+    for k, v in model.state_dict().items():
+        print(k,v)
+        return
+
+
+weights(RN50_Weights[0])
