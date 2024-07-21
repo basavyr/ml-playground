c31a3a69e980176c5820a41abb76592e3f0aee1e
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-07-21 16:10:32 +0300

    add pretrain resnet and print tensors

--- src/pre-trained/pre.py
+++ src/pre-trained/pre.py
@@ -1,6 +1,9 @@
 from torchvision.models import resnet50, ResNet50_Weights
 
 
+# based on the tutorial from pytorch: Models and pre-trained weights
+# source: https://pytorch.org/vision/stable/models.html
+
 RN50_Weights = [ResNet50_Weights.IMAGENET1K_V1,
                 ResNet50_Weights.IMAGENET1K_V2, ResNet50_Weights.DEFAULT, None]
 
@@ -10,7 +13,7 @@ def weights(model_weights: ResNet50_Weights):
     model = resnet50(weights=model_weights)
 
     for k, v in model.state_dict().items():
-        print(k,v)
+        print(k, v)
         return
 
 
