f50c4d5aaa05f08b9b80ffbfa1c14927054f365f
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-07-25 11:06:29 +0300

    add sketch for GPT tutorial from AK

---
+++ tutorials/nano_gpt/train.py
@@ -0,0 +1,5 @@
+# based on the tutorial from Andrew Karpathy
+# source: https://www.youtube.com/watch?v=kCc8FmEb1nY
+
+
+raw_dataset = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
