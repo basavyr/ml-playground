6dac99eb60d042c913f71b2b302cae697364fddf
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-07-25 11:09:18 +0300

    add the training dataset

--- tutorials/nano_gpt/train.py
+++ tutorials/nano_gpt/train.py
@@ -2,4 +2,13 @@
 # source: https://www.youtube.com/watch?v=kCc8FmEb1nY
 
 
-raw_dataset = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+raw_dataset_url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+
+input_file = "input.txt"
+
+
+with open(input_file, 'r') as reader:
+    data = reader.read()
+
+
+print(f'Loaded {len(data)} strings into the buffer')
