e197a3c0e36e7d772fac1778fc70f4e90cf65e9b
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-08-07 10:37:43 +0300

    create Azure specific chat class to be used in external modules

--- tutorials/azure_openai/azure.py
+++ tutorials/azure_openai/azure.py
@@ -10,7 +10,7 @@ def get_azure_env() -> tuple[str, str]:
 
     # try to read the env file manually
     if api_key is None or endpoint is None:
-        print('The Azure API Key and Endpoint are not configured. Please make sure you have .env file with these variables')
+        print('The Azure API Key and Endpoint are not configured. Please make sure you have .env file with these variables ($AZURE_OPENAI_APIKEY , $AZURE_OPENAI_ENDPOINT)')
         try:
             with open(".env", 'r') as reader:
                 data = reader.readlines()
@@ -23,69 +23,79 @@ def get_azure_env() -> tuple[str, str]:
     return api_key, endpoint
 
 
-def prompt(prompt_message: str = "Hey there! Where are you hosted and by which company?", temperature: float = 0.45, max_tokens: int = 1200) -> dict:
-    """
-    - uses the `prompt_message` string to initialize the payload used in the `POST` request to the Azure OpenAI deployment
-    - the payload will contain a `message` object, in which `system`, `user`, and `assistant` contents will be configured
-    - the actual prompt message that a user will require an answer to is provided by the `"role": "user"` content
-    """
-    prompt_obj = {
-        "messages": [
-            {
-                "role": "system",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "You are an AI assistant that helps people find information."
-                    }
-                ]
-            },
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"{prompt_message.strip()}"
-                    }
-                ]
-            },
-            {
-                "role": "assistant",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Hello! How can I assist you today?"
-                    }
-                ]
-            }
-        ],
-        "temperature": temperature,
-        "top_p": 0.95,
-        "max_tokens": max_tokens
-    }
+class AzureChat:
+    def __init__(self, api_key: str, api_endpoint: str):
+        self.api_key = api_key
+        self.endpoint = api_endpoint
+        self.headers = {
+            "Content-Type": "application/json",
+            "api-key": self.api_key,
+        }
 
-    return prompt_obj
+    def generate_prompt_response(self, user_input: str):
+        prompt_message = self.generate_payload(user_input)
+        try:
+            response = requests.post(
+                self.endpoint, headers=self.headers, json=prompt_message)
+            response.raise_for_status()
+        except requests.RequestException as e:
+            raise SystemExit(f"Failed to make the request. Error: {e}")
+
+        if response.status_code in [200, 201, 203]:
+            resp_as_json = response.json()
+            resp_message = resp_as_json["choices"][0]["message"]["content"]
+            return resp_message
+        return None
+
+    def generate_payload(self, user_input: str = "Hey there! Where are you hosted and by which company?", temperature: float = 0.45, max_tokens: int = 1200) -> dict:
+        """
+        - uses the `user_input` string to initialize the payload used in the `POST` request to the Azure OpenAI deployment
+        - the payload will contain a `messages` dictionary, in which `system`, `user`, and `assistant` contents will be configured
+        - the actual prompt message that a user will require an answer to is provided by the `"role": "user"` content
+        """
+        prompt_obj = {
+            "messages": [
+                {
+                    "role": "system",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "You are an AI assistant that helps people find information."
+                        }
+                    ]
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": f"{user_input.strip()}"
+                        }
+                    ]
+                },
+                {
+                    "role": "assistant",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": "Hello! How can I assist you today?"
+                        }
+                    ]
+                }
+            ],
+            "temperature": temperature,
+            "top_p": 0.95,
+            "max_tokens": max_tokens
+        }
+
+        return prompt_obj
 
 
 if __name__ == "__main__":
     API_KEY, API_ENDPOINT = get_azure_env()
 
-    HEADERS = {
-        "Content-Type": "application/json",
-        "api-key": API_KEY,
-    }
+    chat = AzureChat(API_KEY, API_ENDPOINT)
 
-    ######### prompt #########
     MESSAGE = "give me a simple python3 script that will help me interact with you"
-    payload = prompt(MESSAGE)
-    ##########################
 
-    try:
-        response = requests.post(API_ENDPOINT, headers=HEADERS, json=payload)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        raise SystemExit(f"Failed to make the request. Error: {e}")
-
-    resp_as_json = response.json()
-    resp_message = resp_as_json["choices"][0]["message"]["content"]
-    print(resp_message)
+    print(chat.generate_prompt_response(MESSAGE))
