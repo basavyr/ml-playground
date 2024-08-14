2f69a8b46dc8abd8d2fb5326599714e5078caecc
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-08-14 10:38:28 +0300

    use separate system prompt file

--- tutorials/azure_openai/llm_trainer.py
+++ tutorials/azure_openai/llm_trainer.py
@@ -31,46 +31,42 @@ def process_examples(prev_examples: list):
     df.to_json('train.jsonl', orient='records', lines=True)
 
 
-def generate_messages(model_description: str, prev_examples: list):
+def generate_messages(system_prompt: str | None):
+    system_prompt = "You are a helpful assistant." if system_prompt is None else system_prompt
     messages = [
         {
             "role": "system",
-            "content": f"""You are generating data which will be used to train a machine learning model.\n\nYou will be given a high-level description of the model we want to train, and from that, you will generate data samples, each with a prompt/response pair.\n\nYou will do so in this format: prompt-----------$prompt_goes_here-----------response-----------$response_goes_here-----------\n\nOnly one prompt/response pair should be generated per turn.\n\nFor each turn, make the example slightly more complex than the last, while ensuring diversity.\n\nMake sure your samples are unique and diverse, yet high-quality and complex enough to train a well-performing model.\n\nHere is the type of model we want to train:\n{model_description}"""
+            "content": system_prompt,
         }
     ]
-
-    K = 10
-    if len(prev_examples) > 0:
-        if len(prev_examples) >= 10:
-            prev_examples = random.sample(prev_examples, K)
-        for example in prev_examples:
-            messages.append({
-                "role": "assistant",
-                "content": example.strip()
-            })
 
     return messages
 
 
-def main():
-    api_key, api_version, endpoint, deployment = az2.load_env()
+def get_system_prompt(system_prompt_file: str):
+    import os
+    if not os.path.exists(system_prompt_file):
+        return None
+    else:
+        with open(system_prompt_file, 'r+') as reader:
+            data = reader.read()
+            return data
 
+
+def main():
+    system_prompt_file = "gpt-4o.prompt"
+    system_prompt = get_system_prompt(system_prompt_file)
+
+    api_key, api_version, endpoint, deployment = az2.load_env()
     az = az2.AzureInterface(api_key, api_version, endpoint, deployment)
 
-    model_description = """A model that takes in a cybersecurity-oriented question, and responds with a well-reasoned, very short and concise answer. The model is also able to solve coding tasks that are specific to cybersecurity. It is aware of the latest tools that are used in the domain, such as nmap, wfuzz, and many more. Responses related to coding and command specific tasks must only contain the actual code or the actual command to be used."""
-    number_of_examples = 100
-
     start = time.time()
+    messages = generate_messages(system_prompt)
+    example = az.create_chat_completion(messages)
 
-    prev_examples = []
-    for _ in range(number_of_examples):
-        messages = generate_messages(model_description, prev_examples)
-        example = az.create_chat_completion(messages)
-        prev_examples.append(example)
-
-    process_examples(prev_examples)
+    print(example)
     print(
-        f'Generating {number_of_examples} samples took: {round(time.time()-start,2)} s')
+        f'Generating output: {round(time.time()-start,2)} s')
 
 
 if __name__ == "__main__":
