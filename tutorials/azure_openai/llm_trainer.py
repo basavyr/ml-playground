eea7d297d1d51bad24f1b63fb18ec68c8ad5a2c6
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-08-15 13:10:31 +0300

    generate and save prompt separately

--- tutorials/azure_openai/llm_trainer.py
+++ tutorials/azure_openai/llm_trainer.py
@@ -6,7 +6,7 @@ import azure_v2 as az2
 import datetime
 
 
-def generate_messages(system_prompt: str | None):
+def generate_prompt_message(system_prompt: str | None) -> list[dict]:
     system_prompt = "You are a helpful assistant." if system_prompt is None else system_prompt
     messages = [
         {
@@ -18,7 +18,7 @@ def generate_messages(system_prompt: str | None):
     return messages
 
 
-def get_system_prompt(system_prompt_file: str):
+def get_system_prompt(system_prompt_file: str) -> str | None:
     import os
     if not os.path.exists(system_prompt_file):
         return None
@@ -28,13 +28,23 @@ def get_system_prompt(system_prompt_file: str):
             return data
 
 
-def save_model_output(output_dir: str, output_file: str, model_output: str):
+def generate_prompt_response(prompt_message: str) -> str | None:
+    api_key, api_version, endpoint, deployment = az2.load_env()
+
+    az = az2.AzureInterface(api_key, api_version, endpoint, deployment)
+    prompt_response = az.create_chat_completion(prompt_message)
+
+    return prompt_response
+
+
+def save_prompt(output_dir: str, prompt_response: str) -> None:
+    output_file = "az-output"
     now = datetime.datetime.now()
     file_suffix = f'{now.date()}-{now.time()}'[:-7]
     with open(f'{output_dir}/{output_file}-{file_suffix}.jsonl', 'w') as dumper:
-        model_output = model_output.replace(
+        prompt_response = prompt_response.replace(
             "\n", "\\n").split(r"""{"prompt":""")[1:]
-        for line in model_output:
+        for line in prompt_response:
             reconstructed_line = fr"""{{"prompt": {line.strip()}"""
             if reconstructed_line.endswith("\\n\\n"):
                 dumper.write(reconstructed_line[:-len("\\n\\n")])
@@ -45,36 +55,25 @@ def save_model_output(output_dir: str, output_file: str, model_output: str):
             dumper.write("\n")
 
 
-def generate_output_file(output_dir: str, system_prompt: str):
-    model_output_file = "az-output"
-
-    messages = generate_messages(system_prompt)
-
-    api_key, api_version, endpoint, deployment = az2.load_env()
-    az = az2.AzureInterface(api_key, api_version, endpoint, deployment)
-
-    example = az.create_chat_completion(messages)
-
-    save_model_output(output_dir, model_output_file, example)
-
-
 def main():
-    volumes = "./volumes"
+    OUTPUT_FILES = 5  # change the number of output files with data that should be generated
+
+    output_dir = "./volumes"
 
     system_prompt_file = "gpt-4o.prompt"
     system_prompt = get_system_prompt(system_prompt_file)
-
     if system_prompt is None:
         print(
             f'No prompt file available. Please make sure < {system_prompt_file} > is in the correct path.')
         exit(1)
 
+    prompt = generate_prompt_message(system_prompt)
+
     start = time.time()
 
-    OUTPUT_FILES = 5  # change the number of output files with data that should be generated
-
     for idx in range(OUTPUT_FILES):
-        generate_output_file(volumes, system_prompt)
+        prompt_response = generate_prompt_response(prompt)
+        save_prompt(output_dir, prompt_response)
         print(f'Generated {idx+1}/{OUTPUT_FILES} output files')
     print(
         f'Generating {OUTPUT_FILES} files took: {round(time.time()-start,2)} s')
