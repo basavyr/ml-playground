283a8fc2a423dd57697e0ae5bd7ce40e95748780
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-09-08 20:05:14 +0300

    add sum of tensors

--- src/ml-adder/main.py
+++ src/ml-adder/main.py
@@ -6,7 +6,7 @@ from torch.nn import functional as F
 
 relu = F.relu
 
-torch.manual_seed(1137)
+torch.manual_seed(42)
 
 
 def generate_x_and_y(n: int, batch_size: int, highs: int, lows: int = 0):
@@ -14,7 +14,21 @@ def generate_x_and_y(n: int, batch_size: int, highs: int, lows: int = 0):
     return x_y
 
 
+def add_x_and_y(x_y: torch.Tensor):
+    x_values = x_y[:, :, 0]
+    y_values = x_y[:, :, 1]
+    return x_values+y_values
+
+
 if __name__ == "__main__":
     N = 512
     batch_size = 32
     x_y = generate_x_and_y(N, batch_size, 100)
+
+    sum_x_y = add_x_and_y(x_y)
+
+    print(x_y.shape)
+    print(sum_x_y.shape)
+
+    print(x_y[0])
+    print(sum_x_y[0])
