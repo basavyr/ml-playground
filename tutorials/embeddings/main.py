525422bf8920df0bc5b835fe8eac6a6c62b90d2b
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-10-02 15:08:12 +0300

    add support for multiple embeddings

--- tutorials/embeddings/main.py
+++ tutorials/embeddings/main.py
@@ -9,11 +9,10 @@ EMBEDDING_DIM = 10
 
 
 class Embedding(nn.Module):
-    def __init__(self, dictionary: list[str], embedding_dim: int):
+    def __init__(self, num_embeddings: int, embedding_dim: int):
         super(Embedding, self).__init__()
-        self.dictionary = dictionary
         self.embedding_dim = embedding_dim
-        self.embedding = nn.Embedding(len(dictionary), embedding_dim)
+        self.embedding = nn.Embedding(num_embeddings, embedding_dim)
 
     def forward(self, x: torch.Tensor):
         x = self.embedding(x)
@@ -51,14 +50,32 @@ class Tokenizer():
         return torch.tensor(token_ids, dtype=torch.int)
 
 
+def create_word_embedding(tokenizer: Tokenizer, embedding: Embedding, input_string: str):
+    input_token_ids = tokenizer.to_tensor(input_string)
+    word_embedding = embedding(input_token_ids)
+    return word_embedding
+
+
 if __name__ == "__main__":
 
     T = Tokenizer(utils.dictionary)
-    WE = Embedding(utils.dictionary, EMBEDDING_DIM)
+    num_embeddings = len(T.word_indices)
+    WE = Embedding(num_embeddings, EMBEDDING_DIM)
 
-    input = "Hey there, how are you?"
-    input_token_ids = T.to_tensor(input)
+    input_strings = [
+        "Hey there, how are you?",
+        "Hey there, how are you?",
+        "I like coffee and good morning!",
+        "What is your name, please?",
+        "Thanks for coming to the office.",
+        "We will watch a movie tonight.",
+        "How do you code this algorithm?",
+        "I love reading books on a sunny day.",
+        "The computer and keyboard are broken.",
+        "Are you happy with the game results?",
+        "Is the office open today or closed?"
+    ]
 
-    print(input)
-    print(input_token_ids)
-    print(WE(input_token_ids))
+    for input in input_strings:
+        we = create_word_embedding(T, WE, input)
+        print(we)
