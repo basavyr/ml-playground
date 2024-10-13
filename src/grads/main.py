c63763458ee0bbe1d46a48b227d26a367f2eb756
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-10-13 18:27:06 +0300

    add train and eval methods

---
+++ src/grads/main.py
@@ -0,0 +1,111 @@
+
+import torch
+import torch.utils.data
+import torch.nn as nn
+
+import time
+import os
+import sys
+import json
+
+import model as models
+from data import MNIST
+from config import Conv_Configs
+
+
+DEFAULT_DEVICE: torch.device = torch.device("mps") if os.uname(
+).sysname == "Darwin" else torch.device("cpu")
+
+
+def train(training_configs: Conv_Configs):
+    device = training_configs.device
+    loss_fn = training_configs.loss_fn
+    optimizer = training_configs.optimizer
+    data = training_configs.train_loader
+    epochs = training_configs.epochs
+    model = training_configs.model
+
+    training_report = {"training": []}
+    losses = []
+    best_loss = (0, torch.inf)
+    accuracies = []
+    best_accuracy = (0, 0)
+
+    train_start = time.time()
+    model.to(device)
+    model.train()
+    for epoch in range(epochs):
+        start = time.time()
+        epoch_loss = 0
+        predictions = 0
+        for x, y_true in data:
+            x, y_true = x.to(device), y_true.to(device)
+            optimizer.zero_grad()
+
+            y = model(x)
+            loss = loss_fn(y, y_true)
+            epoch_loss += loss.item()
+            predictions += (y.argmax(dim=1) == y_true).sum().item()
+
+            loss.backward()
+            optimizer.step()
+
+        epoch_loss /= len(data)
+        losses.append(epoch_loss)
+        accuracy = predictions/len(data.dataset)*100
+        accuracies.append(accuracy)
+
+        if epoch_loss < best_loss[1]:
+            best_loss = (epoch, epoch_loss)
+        if accuracy > best_accuracy[1]:
+            best_accuracy = (epoch, accuracy)
+
+        training_report["training"].append({
+            "loss": epoch_loss,
+            "acc": accuracy,
+            "epoch_time": time.time()-start,
+            "best_loss": best_loss[1],
+            "best_acc": best_accuracy[1],
+        })
+
+    train_time = time.time()-train_start
+    print(f'Train time: {train_time:.3f} s')
+    print(f'Accuracy: {best_accuracy}')
+    print(f'Loss: {best_loss}')
+
+    training_report["loss"] = best_loss[1]
+    training_report["acc"] = best_accuracy[1]
+    training_report["loss_fn"] = f'{type(loss_fn).__name__}'
+    training_report["optimizer"] = f'{type(optimizer).__name__}'
+
+    with open("training_report.json", 'w') as dumper:
+        json.dump(training_report, dumper)
+
+
+def eval(configs: Conv_Configs):
+    model = configs.model
+    device = configs.device
+    data = configs.eval_loader
+    loss_fn = configs.loss_fn
+
+    start = time.time()
+    model.to(device)
+    model.eval()
+    running_loss = 0
+    for x, y_true in data:
+        x, y_true = x.to(device), y_true.to(device)
+
+        y = model(x)
+        loss = loss_fn(y, y_true)
+        running_loss += loss.item()
+
+    eval_time = time.time()-start
+    print(f'Eval time: {eval_time:.3f} s')
+    print(f'Eval loss = {running_loss/len(data)}')
+
+
+if __name__ == "__main__":
+
+    model_configs = Conv_Configs(MNIST().mnist, DEFAULT_DEVICE)
+
+    train(model_configs)
