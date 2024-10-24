c34dae105eef16f033039b716bb0ee80cd367b8a
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-10-24 12:54:39 +0300

    use SGD instead of Adam

--- src/grads/config.py
+++ src/grads/config.py
@@ -9,11 +9,21 @@ import hashlib
 
 
 class Conv_Configs():
+    adam = torch.optim.Adam
+    sgd = torch.optim.SGD
+    sgd_kwargs = {
+        'lr': 0.001,           # Learning rate
+        'momentum': 0.9,       # Momentum value
+        'weight_decay': 1e-4,  # L2 regularization (weight decay)
+        'dampening': 0,        # Dampening for momentum
+        'nesterov': True       # Use Nesterov momentum
+    }
+
     def __init__(self, data: tuple, device: torch.device, epochs: int = 1, n_layers: int = 3, hidden_units: int = 32, out_channels: int = 8, logits: bool = False) -> None:
         self.model: nn.Module = m.Conv_Model(
             n_layers=n_layers, hidden_units=hidden_units, out_channels=out_channels, logits=logits)
         self.device = device
-        self.optimizer = torch.optim.Adam(self.model.parameters())
+        self.optimizer = self.sgd(self.model.parameters(), **self.sgd_kwargs)
         self.loss_fn = torch.nn.CrossEntropyLoss() if logits else torch.nn.NLLLoss()
         self.epochs = epochs
         self.train_loader, self.eval_loader = data
