81fd958de8084f30dfaaa8d19340fb9227c2479e
Robert Poenaru <robert.poenaru@protonmail.ch>
2024-10-24 14:05:13 +0300

    move `fc0` and `fc1` into `_init_linear_layers`

--- src/grads/model.py
+++ src/grads/model.py
@@ -12,11 +12,7 @@ class Conv_Model(nn.Module):
         self.out_channels = out_channels
         self.hidden_units = hidden_units
         self.logits = logits
-        self.fc0 = nn.Linear(self.out_channels*28*28,
-                             self.hidden_units, bias=False)
-        self.fc1 = nn.Linear(self.hidden_units, 10,
-                             bias=False)  # 10 output classes
-        self.log_softmax = nn.LogSoftmax(dim=1)
+
         self.conv, self.linear = self.make_layers()
 
     def forward(self, x: torch.Tensor):
@@ -25,7 +21,7 @@ class Conv_Model(nn.Module):
         x = self.linear(x)
         if self.logits == True:
             return x
-        return self.log_softmax(x)
+        return nn.functional.log_softmax(x, dim=1)
 
     def _init_conv_layers(self):
         layers = nn.Sequential(OrderedDict([("conv1", nn.Conv2d(
@@ -34,17 +30,21 @@ class Conv_Model(nn.Module):
             conv = nn.Conv2d(self.out_channels, self.out_channels,
                              kernel_size=3, padding="same", bias=False)
             layers.add_module(f'conv{idx+2}', conv)
+            layers.add_module(f'relu{idx+2}', nn.ReLU(inplace=True))
         return layers
 
     def _init_linear_layers(self):
         layers = nn.Sequential(OrderedDict(
-            [("linear1", self.fc0), ("relu1", torch.nn.ReLU(inplace=True))]))
+            [("linear1", nn.Linear(self.out_channels*28*28,
+                                   self.hidden_units, bias=False)), ("relu1", nn.ReLU(inplace=True))]))
+
         for idx in range(self.n_layers-1):
-            if idx == self.n_layers-1:
-                layers.add_module(f'relu{idx+2}', torch.nn.ReLU(inplace=True))
-            else:
-                layers.add_module(f'linear{idx+2}', nn.Linear(self.hidden_units,
-                                                              self.hidden_units, bias=False))
+            layers.add_module(
+                f'linear{idx+2}', nn.Linear(self.hidden_units, self.hidden_units, bias=False))
+            layers.add_module(f'relu{idx+2}', nn.ReLU(inplace=True))
+
+        layers.add_module(f'linear{self.n_layers+1}', nn.Linear(self.hidden_units, 10,
+                                                                bias=False))
         return layers
 
     def make_layers(self):
