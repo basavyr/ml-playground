a122c1eff316070dec0d1e0f7f2a8dae54b4bb1c
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-04 12:24:15 +0200

    improved model configs and moved mask into Transformer class itself

--- benchmarks/transformer.py
+++ benchmarks/transformer.py
@@ -5,18 +5,20 @@ from torch import nn as nn
 import sys
 from tqdm import tqdm
 from torch.utils.data import DataLoader, Dataset
+
 # local imports
 from utils import get_optimal_device
 
 
 class RandomDataset(Dataset):
-    def __init__(self, num_samples: int, sequence_length: int, embedding_dim: int, vocab_size: int):
+    def __init__(self, num_samples: int, sequence_length: int, embedding_dim: int, vocab_size: int, torch_dtype: torch.dtype | None):
         self.num_samples = num_samples
         self.embedding_dim = embedding_dim
         self.sequence_length = sequence_length
-        self.x = torch.randn(num_samples, sequence_length,  embedding_dim)
+        self.x = torch.randn(num_samples, sequence_length,
+                             embedding_dim, dtype=torch_dtype)
         self.y = torch.randint(
-            1, vocab_size, (num_samples, sequence_length))
+            1, vocab_size, (num_samples, sequence_length), dtype=torch.long)
 
     def __len__(self,):
         return len(self.x)
@@ -25,29 +27,36 @@ class RandomDataset(Dataset):
         return self.x[idx], self.y[idx]
 
 
-def create_causal_mask(size: int, device: torch.types.Device):
-    mask = nn.Transformer.generate_square_subsequent_mask(size, device)
-    return mask
-
-
 class Transformer(nn.Module):
-    def __init__(self, d_model: int, n_layers: int, n_heads: int, vocab_size: int):
+    def __init__(self, d_model: int, n_layers: int, n_heads: int, vocab_size: int, torch_dtype: torch.dtype | None):
         super(Transformer, self).__init__()
         self.decoder_layer = nn.TransformerDecoderLayer(d_model=d_model,
-                                                        nhead=n_heads, batch_first=True)
+                                                        nhead=n_heads, batch_first=True, dtype=torch_dtype)
         self.decoder = nn.TransformerDecoder(
             decoder_layer=self.decoder_layer, num_layers=n_layers)
-        self.lm_head = nn.Linear(d_model, vocab_size)
+        self.lm_head = nn.Linear(d_model, vocab_size, dtype=torch_dtype)
         self.d_model = d_model
+        self.n_layers = n_layers
+        self.n_heads = n_heads
+        self.vocab_size = vocab_size
+        self.torch_dtype = torch_dtype
 
-    def forward(self, tgt: torch.Tensor, tgt_mask: torch.Tensor):
-        memory = torch.zeros(tgt.shape[0], 0, self.d_model).to(tgt.device)
+    def forward(self, tgt: torch.Tensor):
+        memory = torch.zeros(
+            tgt.shape[0], 0, self.d_model, dtype=tgt.dtype).to(tgt.device)
+        tgt_mask = self.create_causal_mask(tgt)
         decoder_output = self.decoder(tgt=tgt,
                                       memory=memory,
                                       tgt_mask=tgt_mask)
         logits = self.lm_head(decoder_output)
 
         return logits
+
+    def create_causal_mask(self, tgt: torch.Tensor):
+        # tgt shape: B, S, D_MODEL
+        mask = nn.Transformer.generate_square_subsequent_mask(
+            sz=tgt.shape[1], device=tgt.device, dtype=tgt.dtype)
+        return mask
 
 
 def train_model(
@@ -67,12 +76,9 @@ def train_model(
         epoch_loss = 0.0
         for x, y_true in tqdm(train_loader, desc=f'Epoch {epoch+1}: Training transformer'):
             x, y_true = x.to(device), y_true.to(device)
-            mask = create_causal_mask(x.shape[1], device)
-
             optimizer.zero_grad()
 
-            # TODO: a target mask should be applied
-            y = model(x, mask)  # torch.Size([128, 32, 30000])
+            y = model(x)  # torch.Size([128, 32, 30000])
 
             # shift targets and logits
             y_shifted = y[:, :-1, :].contiguous()
@@ -97,24 +103,32 @@ def main():
     loss_fn = nn.CrossEntropyLoss()
     num_epochs = 10
     learning_rate = 1e-4
-
-    # data configs
-    num_samples = 10000
-    vocab_size = 30000
-    batch_size = 8
-    sequence_length = 32
-    embedding_dim = 384
+    torch_dtype = torch.float16  # TODO test with bfloat16
 
     # model configs
-    d_model = embedding_dim
+    d_model = 384
     n_layers = 6
     n_heads = 8
 
-    train_loader = DataLoader(RandomDataset(
-        num_samples, sequence_length, embedding_dim, vocab_size), batch_size=batch_size, shuffle=False)
+    # data configs
+    embedding_dim = d_model
+    vocab_size = 30000
+    num_samples = 10000
+    batch_size = 32
+    sequence_length = 128
 
-    model = Transformer(d_model=d_model, n_layers=n_layers,
-                        n_heads=n_heads, vocab_size=vocab_size)
+    dataset = RandomDataset(num_samples=num_samples,
+                            sequence_length=sequence_length,
+                            embedding_dim=embedding_dim,
+                            vocab_size=vocab_size,
+                            torch_dtype=torch_dtype)
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+    model = Transformer(d_model=d_model,
+                        n_layers=n_layers,
+                        n_heads=n_heads,
+                        vocab_size=vocab_size,
+                        torch_dtype=torch_dtype)
 
     train_model(model=model,
                 device=device,
