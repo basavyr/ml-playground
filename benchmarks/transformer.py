8e8ad1704be9bd19a22a8ff0a7364b8a1c2adb62
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-04 14:12:25 +0200

    add improved logging

--- benchmarks/transformer.py
+++ benchmarks/transformer.py
@@ -1,10 +1,11 @@
-from typing import Callable
 import torch
 from torch import nn as nn
-
-import sys
-from tqdm import tqdm
 from torch.utils.data import DataLoader, Dataset
+
+from typing import Callable
+import sys
+import time
+from tqdm import tqdm
 
 # local imports
 from utils import get_optimal_device, log
@@ -61,10 +62,13 @@ def train_model(
     model.to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
+    train_start = time.monotonic_ns()
+    training_times = []
     for epoch in range(num_epochs):
         model.train()
 
         epoch_loss = 0.0
+        epoch_start = time.monotonic_ns()
         for x, y_true in tqdm(train_loader, desc=f'Epoch {epoch+1}: Training transformer'):
             x, y_true = x.to(device), y_true.to(device)
             memory = torch.zeros(
@@ -87,16 +91,23 @@ def train_model(
 
             loss.backward()
             optimizer.step()
+        epoch_time = (time.monotonic_ns()-epoch_start)*1e-9
+        training_times.append(epoch_time)
 
         epoch_loss /= len(train_loader.dataset)
-        log.info(f'Epoch {epoch+1}: Loss= {epoch_loss:.3f}')
+        log.info(
+            f'Epoch {epoch+1}: Loss= {epoch_loss:.3f} [{epoch_time:3f} s]')
+
+    training_time = (time.monotonic_ns()-train_start)*1e-9
+    log.info(
+        f'Full training finished in {training_time:.3f} s ({sum(training_times)/len(training_times):.3f} s per epoch)')
 
 
 def main():
     # training configs
     device = get_optimal_device()
     loss_fn = nn.CrossEntropyLoss()
-    num_epochs = 10
+    num_epochs = 3
     learning_rate = 1e-4
     torch_dtype = torch.float32  # TODO test with bfloat16
 
@@ -119,18 +130,34 @@ def main():
                             torch_dtype=torch_dtype)
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
+    _info = f'Training on {device} for {num_epochs} epochs.\n<<< Config >>>\nBatch Size={batch_size} | Total samples={num_samples}\nSequence Length={sequence_length}\nN_decoder_layers={n_layers} | num_attn_heads={n_heads} | d_k={d_model}\n{"="*80}'
+    log.info(_info)
     model = Transformer(d_model=d_model,
                         n_layers=n_layers,
                         n_heads=n_heads,
                         vocab_size=vocab_size,
                         torch_dtype=torch_dtype)
 
+    train_start = time.monotonic_ns()
     train_model(model=model,
                 device=device,
                 loss_fn=loss_fn,
                 train_loader=train_loader,
                 num_epochs=num_epochs,
                 learning_rate=learning_rate)
+    train_duration = (time.monotonic_ns() - train_start)*1e-9
+
+    flops_per_attention = 4*sequence_length * \
+        d_model**2 + 2*sequence_length**2*d_model
+    flops_per_ffn = 2*sequence_length*d_model*2048
+    flops_per_lm_head = sequence_length*d_model*vocab_size
+
+    flops_per_layer = flops_per_attention + flops_per_ffn + flops_per_lm_head
+    flops_per_batch = batch_size * n_layers * flops_per_layer
+    flops_total = 3 * flops_per_batch * \
+        ((num_samples + batch_size - 1) // batch_size) * num_epochs
+    log.info(f'Total operations: {flops_total/1e12:.4f} TFLOPs')
+    log.info(f'Achieved avg. << {(flops_total/1e12)/train_duration:.4f} >> TFLOPS')
 
 
 if __name__ == "__main__":
