1fa37819b5dbed8d927c2074ab91dad137715d5a
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-07 14:36:59 +0200

    add flop calculator via built-in pytorch

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -21,6 +21,40 @@ def get_optimal_device(force_cuda: bool = False) -> Device:
         return torch.device("cpu")
 
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
-log = logging.getLogger()
+
+def get_flops(
+    model: torch.nn.Module,
+    device: Device,
+    custom_input: Tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+    is_custom_transformer: bool = False,
+    with_backward: bool = False
+) -> float:
+    model.to(device)
+    is_training = model.training
+    model.eval()
+
+    if is_custom_transformer:
+        tgt, mem, tgt_mask = custom_input
+        tgt, mem, tgt_mask = tgt.to(device), mem.to(
+            device), tgt_mask.to(device)
+    else:
+        tgt, _, _ = custom_input
+        tgt = tgt.to(device)
+        mem = None
+        tgt_mask = None
+
+    flop_counter = FlopCounterMode(display=False)
+    with flop_counter:
+        if is_custom_transformer:
+            if with_backward:
+                model(tgt, mem, tgt_mask).sum().backward()
+            else:
+                model(tgt, mem, tgt_mask)
+        else:
+            if with_backward:
+                model(tgt).sum().backward()
+            else:
+                model(tgt)
+    if is_training:
+        model.train()
+    return flop_counter.get_total_flops()
