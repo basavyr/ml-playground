42d0cf00a7f6148c4c42e9aef24a60f3b2ed54b7
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-07 14:36:40 +0200

    move logging

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -1,10 +1,15 @@
-import logging
 import torch
-
 from torch.types import Device
+from torch.utils.flop_counter import FlopCounterMode
+from typing import Tuple
+
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+log = logging.getLogger()
 
 
-def get_optimal_device(force_cuda: bool = False):
+def get_optimal_device(force_cuda: bool = False) -> Device:
     if force_cuda and not torch.cuda.is_available():
         raise ModuleNotFoundError(
             "Cannot use CUDA device! Make sure CUDA is available on the system.")
