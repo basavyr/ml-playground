a4fa0ca792801088c7eb0899315fe5862db73ff0
Robert Poenaru <robert.poenaru@protonmail.ch>
2025-12-07 14:38:17 +0200

    fix the formula for approximating FLOPS

--- benchmarks/utils.py
+++ benchmarks/utils.py
@@ -21,6 +21,44 @@ def get_optimal_device(force_cuda: bool = False) -> Device:
         return torch.device("cpu")
 
 
+def get_flops_approx(
+    d_model: int,
+    n_layers: int,
+    n_heads: int,
+    sequence_length: int,
+    vocab_size: int,
+    num_samples: int,
+    num_epochs: int,
+    d_ffn: int = 2048,
+) -> float:
+    # decoder
+    """
+    in terms of param count, the attentions are:
+    3 projections (QKV) with [ sequence_length * d_model^2 ] -> 3 * sequence_length * d_model^2
+    1 output projection (O) with [ sequence_length * d_model^2] -> 1 * sequence_length * d_model^2
+    total for param count: 4 * sequence_length * d_model^2
+
+    in terms of flops:
+    2 * p
+    """
+    flops_per_attention = 8 * sequence_length * \
+        (d_model**2) + 4 * (sequence_length**2) * \
+        d_model + 3 * n_heads * (sequence_length**2)
+    flops_per_ffn = 2 * (2 * sequence_length * d_model * d_ffn)
+
+    # lm head
+    flops_per_lm_head = 2 * sequence_length * \
+        d_model*vocab_size  # the 2*M*N*K rule
+
+    # per 1 forward pass
+    flops_model_1fp = n_layers * \
+        (flops_per_attention + flops_per_ffn)+flops_per_lm_head
+
+    # per data and backward (1xfp + 2xbp = 3x)
+    flops_iter = 3 * num_samples * flops_model_1fp
+
+    return num_epochs*flops_iter
+
 
 def get_flops(
     model: torch.nn.Module,
