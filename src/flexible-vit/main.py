afc8d409520a4f351e984816069c24883fc34310
Robert Poenaru <robert.poenaru@protonmail.ch>
2026-01-20 12:21:39 +0200

    add finetuning implementation

--- src/flexible-vit/main.py
+++ src/flexible-vit/main.py
@@ -70,8 +70,61 @@ def train_vit(vit_model: nn.Module, patch_size: int, training_config: TrainingCo
     print(f'Final loss: {epoch_loss} | Checkpoint -> {model_pth}')
 
 
-def finetune_vit(model_checkpoint: str, finetuning_config: TrainingConfig, checkpoint_data_config: DataConfig, new_data_config: DataConfig):
-    raise NotADirectoryError("TBD")
+def finetune_vit(model_checkpoint: str, finetuning_config: TrainingConfig, checkpoint_data_config: DataConfig, finetuning_data_loader: DataLoader, finetuning_data_config: DataConfig):
+    assert os.path.isfile(
+        model_checkpoint), f"Incorrect model checkpoint: {model_checkpoint}"
+    full_model_dict = torch.load(
+        model_checkpoint, weights_only=False, map_location="cpu")
+
+    old_patch_size = get_patch_size_vit(checkpoint_data_config.img_size)
+    new_patch_size = get_patch_size_vit(finetuning_data_config.img_size)
+    model = VisionTransformer(img_size=checkpoint_data_config.img_size,
+                              patch_size=old_patch_size,
+                              in_channels=checkpoint_data_config.in_channels,
+                              num_classes=checkpoint_data_config.num_classes)
+    model.load_state_dict(full_model_dict['state_dict'])
+    if old_patch_size != new_patch_size:
+        print(f'Old P={old_patch_size} -> New P={new_patch_size}')
+        model.set_input_size(finetuning_data_config.img_size, new_patch_size)
+        model.reset_classifier(finetuning_data_config.num_classes)
+    model.train()
+    model.to(finetuning_config.device)
+
+    optimizer = torch.optim.SGD(model.parameters(
+    ), lr=finetuning_config.lr, momentum=0.9, nesterov=True, weight_decay=1e-4)
+    loss_fn = nn.CrossEntropyLoss()
+
+    pbar = tqdm(total=finetuning_config.epochs *
+                finetuning_data_config.num_samples, desc=f'Finetuning ViT on {finetuning_data_config.dataset_type}')
+    epoch_loss = None
+    for _ in range(finetuning_config.epochs):
+        epoch_loss = 0.0
+        num_samples = 0
+        for x, y_true in finetuning_data_loader:
+            x, y_true = x.to(finetuning_config.device), y_true.to(
+                finetuning_config.device)
+            optimizer.zero_grad()
+
+            y = model(x)
+            loss = loss_fn(y, y_true)
+            epoch_loss += loss.item()*x.shape[0]
+            num_samples += x.shape[0]
+
+            loss.backward()
+            optimizer.step()
+
+            pbar.update(x.shape[0])
+
+    pbar.close()
+    os.makedirs("./models", exist_ok=True)
+    model_pth = f'FT_vit-img{finetuning_data_config.img_size}-p{new_patch_size}_{finetuning_config.epochs}epochs.pt'
+    model = {
+        "state_dict": model.state_dict(),
+        "training_config": finetuning_config,
+        "data_config": finetuning_data_config,
+    }
+    torch.save(model, f'models/{model_pth}')
+    print(f'Final loss: {epoch_loss} | Finetuning checkpoint -> {model_pth}')
 
 
 def main():
@@ -79,24 +132,30 @@ def main():
     training_config = TrainingConfig(
         device=device,
         batch_size=128,
-        epochs=1,
+        epochs=2,
         seed=1137)
     set_deterministic_behavior(training_config.seed)
 
-    dataset_type = "mnist"
+    dataset_type = "tiny"
     trainloader, data_config = get_dataloader_and_config(
         dataset_type=dataset_type,
-        num_samples=1000,
-        batch_size=128,
+        num_samples=2000,
+        batch_size=256,
         train=True)
 
-    patch_size = get_patch_size_vit(data_config.img_size)
-    vit = VisionTransformer(img_size=data_config.img_size,
-                            patch_size=patch_size,
-                            in_channels=data_config.in_channels,
-                            num_classes=data_config.num_classes,
-                            dynamic_img_size=True)
-    train_vit(vit, patch_size, training_config, data_config, trainloader)
+    # patch_size = get_patch_size_vit(data_config.img_size)
+    # vit = VisionTransformer(img_size=data_config.img_size,
+    #                         patch_size=patch_size,
+    #                         in_channels=data_config.in_channels,
+    #                         num_classes=data_config.num_classes,
+    #                         dynamic_img_size=False)
+    # train_vit(vit, patch_size, training_config, data_config, trainloader)
+
+    # finetuning
+    finetuning_data_loader, finetuning_data_config = get_dataloader_and_config(
+        "mnist", 1000, 128, True)
+    finetune_vit("models/vit-img64-p8_2epochs.pt", training_config,
+                 data_config, finetuning_data_loader, finetuning_data_config)
 
 
 if __name__ == "__main__":
